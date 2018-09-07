ae42d56848474bbcf5100c1fcaf24925b8c134a0
jasonsuzhou <jason.yao525@gmail.com>
2018-09-07 23:36:53 +0800

    add aliyun mq config

--- src/main/java/com/xuya/charge/phone/infra/aliyunmq/AliyunMQSenderConfig.java
+++ src/main/java/com/xuya/charge/phone/infra/aliyunmq/AliyunMQSenderConfig.java
@@ -1,7 +1,6 @@
 package com.xuya.charge.phone.infra.aliyunmq;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,16 +8,18 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AliyunMQSenderConfig {
 
-	@Value("${aliyun.mq.accesskey}")
-	private String accessKey;
-	@Value("${aliyun.mq.secretkey}")
-	private String secretKey;
+	@Value("${aliyun.mq.accesskey}") String accessKey;
+	@Value("${aliyun.mq.secretkey}") String secretKey;
+	@Value("${aliyun.mq.topic.order}") String orderTopic;
+	@Value("${aliyun.mq.topic.order.pid}") String orderTopicProducer;
+	@Value("${aliyun.mq.topic.callback}") String callbackTopic;
+	@Value("${aliyun.mq.topic.callback.pid}") String callbackTopicProducer;
 
 	@Bean
 	public AliyunMQMsgSender orderCreatedSender() {
 		if (StringUtils.isNotBlank(accessKey) && StringUtils.isNotBlank(secretKey)) {
-			AliyunMQMsgSender sender = new AliyunMQMsgSender("FLUX_CALLBACK");
-			sender.initProducer("PID_FLUX_CALLBACK", accessKey, secretKey);
+			AliyunMQMsgSender sender = new AliyunMQMsgSender(orderTopic);
+			sender.initProducer(orderTopicProducer, accessKey, secretKey);
 			return sender;
 		} else {
 			return new AliyunMQMsgSender();
@@ -28,8 +29,8 @@ public class AliyunMQSenderConfig {
 	@Bean
 	public AliyunMQMsgSender providerCallbackSender() {
 		if (StringUtils.isNotBlank(accessKey) && StringUtils.isNotBlank(secretKey)) {
-			AliyunMQMsgSender sender = new AliyunMQMsgSender("FLUX_CALLBACK");
-			sender.initProducer("PID_FLUX_CALLBACK", accessKey, secretKey);
+			AliyunMQMsgSender sender = new AliyunMQMsgSender(callbackTopic);
+			sender.initProducer(callbackTopicProducer, accessKey, secretKey);
 			return sender;
 		} else {
 			return new AliyunMQMsgSender();

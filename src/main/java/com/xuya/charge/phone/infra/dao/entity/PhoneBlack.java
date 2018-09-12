ea83548cd2215045001afc91fff4e9c173480e83
jasonsuzhou <jason.yao525@gmail.com>
2018-09-12 21:00:02 +0800

    bug fix

--- src/main/java/com/xuya/charge/phone/infra/dao/entity/PhoneBlack.java
+++ src/main/java/com/xuya/charge/phone/infra/dao/entity/PhoneBlack.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "phone_black")
+@Table(name = "conf_phone_black")
 public class PhoneBlack implements Serializable {
 
 	/**
